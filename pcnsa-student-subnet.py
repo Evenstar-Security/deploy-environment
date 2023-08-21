f6cd2eb89ae306705d0eecd2e6187f6aeea1293b
Wesley Belleman <bellemanwesley@gmail.com>
2023-08-20 20:46:39 -0400

    boto3 session

--- pcnsa-student-subnet.py
+++ pcnsa-student-subnet.py
@@ -1,1 +1,9 @@
 import boto3
+
+with open("../keys/aws_cli.txt","r") as f:
+    keys = f.readlines()
+
+session = boto3.Session(
+    aws_access_key_id=keys[0],
+    aws_secret_access_key=keys[1],
+)
