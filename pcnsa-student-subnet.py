e793f429165d490e1f5e813153ec681ee731ae85
Wesley Belleman <bellemanwesley@gmail.com>
2023-08-22 19:10:08 -0400

    testing

--- pcnsa-student-subnet.py
+++ pcnsa-student-subnet.py
@@ -1,4 +1,7 @@
 import boto3
+import sys
+
+instance_id = sys.argv[1]
 
 with open("../keys/aws_cli.txt","r") as f:
     keys_str = f.read()
@@ -8,6 +11,15 @@ keys = keys_str.split("\n")
 session = boto3.Session(
     aws_access_key_id=keys[0],
     aws_secret_access_key=keys[1],
+)
+
+ec2_client = session.client('ec2')
+
+ec2_client.start_instances(
+    InstanceIds=[
+        instance_id,
+    ],
+    DryRun=True
 )
 
 print(keys)
